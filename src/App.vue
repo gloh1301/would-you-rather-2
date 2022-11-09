7a0ca5c8a2eaaf0bc09066f8c81c4fbb443d0923
Grant Lohman <grant_d_lohman@aol.com>
2022-11-08 18:18:07 -0600

    second would you rather vue app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+
+      <h1>Would You Rather?</h1>
+
+      <!-- gets the template from WouldYouRather.vue -->
+      <!-- v-for to loop over questions array -->
+      <!-- v-bind for questions and answers -->
+      <would-you-rather 
+      v-for="question in questions" 
+      v-bind:question="question.question"
+      v-bind:answer1="question.answer1"
+      v-bind:answer2="question.answer2"
+      v-on:answer-changed="answerChanged">
+      </would-you-rather>
+
+      <h1>You Would Rather...</h1>
+
+      <!-- loops to display each choice, not sure why none are displaying-->
+      <p v-for="question in questions">{{ choice }}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// connects the WouldYouRather component
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+
+  // questions array data
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: 'live in a house shaped like a triangle or a house shaped like a circle?',
+          answer1: 'Live in a triangle house',
+          answer2: 'Live in a circle house'
+        },
+        {
+          id: 1,
+          question: 'be a wizard or a superhero?',
+          answer1: 'Be a wizard',
+          answer2: 'Be a superhero'
+        },
+        {
+          id: 2,
+          question: 'have an elephant-sized cat or a cat-sized elephant?',
+          answer1: 'Have an elephant-sized cat',
+          answer2: 'Have a cat-sized elephant'
+        },
+      ]
+    }
+  },
+  methods: {
+
+    // funcion to display answer1 or answer2 depending on which radio button is pushed
+    // not sure if this will work having trouble getting answers to display
+    answerChanged(choice) {
+      if (this.choice = answer1) {
+        this.choice.push(answer1)
+      } else {
+        this.choice.push(answer2)
+      }
+    }
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2C3E50;
+  margin-top: 20px;
+  padding-top: 20px;
+  padding-bottom: 30px;
+  background: lightseagreen;
 }
+
 </style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wyrm">
+
+    <h2>Would you rather...</h2>
+
+    <!-- bind value of question to h3 -->
+    <h3 v-bind:value="question">{{ question }}</h3>
+
+    <!-- radio buttons with answers bound to them with listeners for when they are changed -->
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:change="choiceMade">
+    <label>{{ answer1 }}</label>
+
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:change="choiceMade">
+    <label>{{ answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRather',
+  props: {
+    id: Number,
+    question: String,
+    answer1: String,
+    answer2: String,
+  },
+  data() {
+    return {
+      choice: ''
+    }
+  },
+  methods: {
+
+    // choices emitted to the parent, app.vue
+    choiceMade() {
+      this.$emit('answer-changed', this.choice)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin-bottom: 20px;
+}
+
+.wyrm {
+  border: 2px black solid;
+  background: linear-gradient(violet,lightskyblue);
+  text-align: left;
+  margin: 25px 20px 25px 20px;
+  padding-left: 10px;
+  padding-bottom: 10px;
+}
+
+</style>
